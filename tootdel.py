238c6978c9761e27da27cbb76d424a260bd6c05d
Cobalt Committee <mail@cobaltcommittee.me>
2021-02-05 18:33:57 -0600

    Removed saving the last toot id processed
    This is intended to run at a regular interval and keep a set number of days worth of toots. Seemed pointless to save the last toot id and use that as the starting point for the next run as it really isn’t going to save much effort.

--- tootdel.py
+++ tootdel.py
@@ -1,7 +1,6 @@
 from mastodon import Mastodon
 from datetime import timedelta, datetime, timezone
 from pathlib import Path
-import os
 import time
 import logging
 
@@ -11,23 +10,6 @@ def deletetoot(last_toot_id):
     logging.debug('deleted toot: ' + str(last_toot_id))
     logging.debug('sleeping for 5 seconds...')
     time.sleep(5)
-
-def save_last_position(path, last_toot_id):
-    logging.debug('saving last position')
-    last_toot = Path(path,'last_toot.txt')
-    if last_toot.exists():
-        os.remove(last_toot)
-    last_toot.open(mode='w')
-    last_toot.write_text(str(last_toot_id))
-
-def load_last_position(path):
-    logging.debug('loading last toot id')
-    last_toot = Path(Path.home(), Path('.tootdel'),'last_toot.txt')
-    if not last_toot.exists():
-        return str(0)
-    else:
-        last_toot.open(mode='r')
-        return last_toot.read_text()
 
 if __name__ == '__main__':
 
@@ -39,7 +21,6 @@ if __name__ == '__main__':
     today = datetime.now(tz=timezone.utc)
     delete_prior_to = today - timedelta(days=days_to_keep)
     keep_looping = True
-    last_toot_id = ''
     number_toots_deleted = 0
 
     p = Path(Path.home(), Path('.tootdel'))
@@ -55,33 +36,26 @@ if __name__ == '__main__':
 
     logging.debug('deleting toots prior to: ' + str(delete_prior_to))
 
-    last_toot_id = load_last_position(p)
-    logging.debug('loaded last toot id: ' + last_toot_id)
-
     logging.debug('retrieving toots...')
-    if last_toot_id == '0':
-        results = m.account_statuses(account_id)
-    else:
-        results = m.account_statuses(account_id, min_id=last_toot_id)
+    results = m.account_statuses(account_id)
 
     logging.debug('deleting toots older than ' + str(days_to_keep) + ' days')
     while keep_looping:
         if len(results) > 0:
             for item in results:
                 toot_date = item["created_at"]
-                last_toot_id = item["id"]
+                toot_id = item["id"]
                 if toot_date < delete_prior_to:
-                    deletetoot(last_toot_id)
+                    deletetoot(toot_id)
                     number_toots_deleted += 1
             logging.debug('Sleeping for 5 seconds before retrieving more toots...')
             time.sleep(5)
             if number_toots_deleted == 0:
                 logging.debug('No toots deleted in current batch')
             logging.debug('Retrieving more toots')
-            results = m.account_statuses(account_id, max_id=last_toot_id)
+            results = m.account_statuses(account_id, max_id=toot_id)
             logging.debug('loaded ' + str(len(results)) + ' toots')
         else:
             keep_looping = False
 
     logging.debug('deleted ' + str(number_toots_deleted) + ' toots')
-    save_last_position(p,last_toot_id)
